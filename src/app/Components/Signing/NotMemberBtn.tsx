4c8541328d0a34cc277c7969075ef5e641d1b705
Shlomi Shitrit <akunh113@gmail.com>
2023-09-07 15:38:38 +0300

    little fix

--- src/app/Components/Signing/NotMemberBtn.tsx
+++ src/app/Components/Signing/NotMemberBtn.tsx
@@ -3,12 +3,12 @@ import { NotMemberBtnProps } from "@/app/Interfaces/interfaces";
 function NotMemberBtn({ signUpRouteHandler = () => null }: NotMemberBtnProps) {
     return (
         <p className="mt-10 text-center text-sm text-gray-400">
-            Not a member?
+            Not a member? &nbsp;
             <button
                 onClick={signUpRouteHandler}
                 className="font-semibold leading-6 text-indigo-400 hover:text-indigo-300"
             >
-                Sign Up
+                 Sign Up
             </button>
         </p>
     );
