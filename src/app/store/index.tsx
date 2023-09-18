43c40b8090629bad366183cdda3f24e6919d44c3
Shlomi Shitrit <akunh113@gmail.com>
2023-09-19 00:49:58 +0300

    register isState slice to store

--- src/app/store/index.tsx
+++ src/app/store/index.tsx
@@ -3,6 +3,7 @@ import { ordersSlice } from "./orders";
 import { yearSlice } from "./year";
 import { categoriesSlice } from "./categories";
 import { namesSlice } from "./names";
+import { isStateSlice } from "./isState";
 
 const store = configureStore({
     reducer: {
@@ -10,6 +11,7 @@ const store = configureStore({
         year: yearSlice.reducer,
         categories: categoriesSlice.reducer,
         names: namesSlice.reducer,
+        isState: isStateSlice.reducer,
     },
 });
 
