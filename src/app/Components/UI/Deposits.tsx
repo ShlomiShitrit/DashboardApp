68be937e050efd9b1dfe4aed992e69ae2e6a502b
Shlomi Shitrit <akunh113@gmail.com>
2023-09-19 11:24:36 +0300

    little fix

--- src/app/Components/UI/Deposits.tsx
+++ src/app/Components/UI/Deposits.tsx
@@ -3,20 +3,17 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { yearActions } from "@/app/store/year";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import dayjs from "dayjs";
 import { SelectChangeEvent } from "@mui/material/Select";
 import Box from "@mui/material/Box";
 import SelectComp from "@/app/Components/Form/SelectComp";
-import { DepositsProps, UserInfo } from "../../GeneralResources/interfaces";
+import { UserInfo } from "@/app/GeneralResources/interfaces";
 import { getDataFromDB } from "@/app/Firebase/firebaseFunc";
 import {
     depositsTyp1Style,
     depositsTyp2Style,
     depositsTyp3Style,
-    depositsBtnStyle,
-    depositsBtnBoxStyle,
-} from "../../GeneralResources/styles";
+} from "@/app/GeneralResources/styles";
 import {
     DEPOSITS_DATE_FORMAT,
     DEPOSITS_TYP1_VAR,
@@ -24,16 +21,11 @@ import {
     DEPOSITS_TYP2_VAR,
     DEPOSITS_TYP2_COLOR,
     DEPOSITS_TYP2_TXT,
-    DEPOSITS_BTN_COLOR,
-    DEPOSITS_BTN_VAR,
-    DEPOSITS_BUDGET_BTN_TXT,
     DEPOSITS_START_YEAR,
     DEPOSITS_TYP3_TXT,
     DEPOSITS_TYP3_VAR,
     DEPOSITS_TYP3_COLOR,
-    DEPOSITS_CATEHORY_BTN_TXT,
     DEPOSITS_SELECT_COMP_HEIGHT,
-    DEPOSITS_NAMES_BTN_TXT,
     DEPOSITS_EMPTY_STR,
     FB_USER_INFO_URL,
     DEPOSITS_BUDGET_PARAM,
